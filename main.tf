d4357ae0ce3b7bd77b4a69a6400c479fdb9b31bc
AlamakMacNovoCosure <cohyle9@gmail.com>
2024-02-17 22:31:35 +0800

    initial commit

--- main.tf
+++ main.tf
@@ -1,12 +1,54 @@
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "5.37.0"
+resource "aws_ecr_repository" "app_ecr_repo" {
+  name = "app_repo"
+}
+
+resource "aws_ecs_cluster" "joseph_cluster" {
+  name = "app-cluster"
+}
+
+resource "aws_ecs_task_definition" "app_task" {
+  family                   = "app-first-task" # Name your task
+  container_definitions    = <<DEFINITION
+  [
+    {
+      "name": "app-first-task",
+      "image": "${aws_ecr_repository.app_ecr_repo.repository_url}",
+      "essential": true,
+      "portMappings": [
+        {
+          "containerPort": 5000,
+          "hostPort": 5000
+        }
+      ],
+      "memory": 512,
+      "cpu": 256
+    }
+  ]
+  DEFINITION
+  requires_compatibilities = ["FARGATE"] # use Fargate as the launch type
+  network_mode             = "awsvpc"    # add the AWS VPN network mode as this is required for Fargate
+  memory                   = 512         # Specify the memory the container requires
+  cpu                      = 256         # Specify the CPU the container requires
+  execution_role_arn       = "${aws_iam_role.ecsTaskExecutionRole1.arn}"
+}
+
+resource "aws_iam_role" "ecsTaskExecutionRole1" {
+  name               = "ecsTaskExecutionRole1"
+  assume_role_policy = "${data.aws_iam_policy_document.assume_role_policy.json}"
+}
+
+data "aws_iam_policy_document" "assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
     }
   }
 }
 
-provider "aws" {
-  region = "ap-southeast-1"
+resource "aws_iam_role_policy_attachment" "ecsTaskExecutionRole1_policy" {
+  role       = "${aws_iam_role.ecsTaskExecutionRole1.name}"
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
